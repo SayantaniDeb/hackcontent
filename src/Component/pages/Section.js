2e94c88aff7cb75601105cc73b20874bb04c6222
Sayantani Deb <sayantani.1001@gmail.com>
2023-03-03 23:34:10 +0530

    initial commit

---
+++ src/Component/pages/Section.js
@@ -0,0 +1,31 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+import logo from '../../images/11.gif'
+import NavComponent from './Navbar'
+
+function Section() {
+  return (
+    <>
+      <NavComponent/>
+      <div className="flex flex-col items-center md:flex-row justify-center md:justify-between">
+        <img src={logo} alt="Logo" className="w-full md:mt-16 md:w-1/2 mx-4" />
+        <div className="text-xl text-center mt-16 md:mt-4 md:text-left md:ml-4 md:text-left md:pr-16 md:text-left md:text-left lg:text-left">
+         <h1 className='text-4xl'>
+          Start creating with ease
+        </h1>
+        <h4>
+        Let us guide your content creation journey and make it easy for you.
+        </h4>
+        <Button className='mx-2 mt-4'>Idea</Button>
+        <Button className='mx-2 mt-4'>Voice-over</Button>
+        <Button className='mx-2 mt-4'>Context</Button>
+        <Button className='mx-2 mt-4'>Hashtag</Button>
+        </div>
+      </div>
+      
+
+    </>
+  )
+}
+
+export default Section
